46e9f0e55800c62be12aa3ea6ee0ea96be982b85
“Meiyin <meiyindeng@Meiyins-MacBook-Pro.local>
2020-10-24 22:28:21 -0700

    promise part2 spinner

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -10,17 +10,22 @@ export class PromotionService {
 
   constructor() { }
   getPromotions(): Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS);
+    return new Promise( resolve => {
+      setTimeout(() => resolve(PROMOTIONS), 2000);
+    });  
   }
 
   //return the 1st promotion dish given the pass-in id
   getPromotion(id: string): Promise<Promotion>{
-    return Promise.resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]);
+    return new Promise( resolve => {
+      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
+    });  
   }
 
   //return the 1st promotion dish which featured = true
   getFeaturedPromotion(): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => promo.featured)[0]);
-
+    return new Promise (resolve => {
+      setTimeout(() => resolve(PROMOTIONS.filter((promo) => promo.featured)[0]), 2000);
+    });
   }
 }
