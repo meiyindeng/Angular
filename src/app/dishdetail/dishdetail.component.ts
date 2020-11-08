a86363a967a8e2d066b5bdf1636d147c55fe0624
“Meiyin <meiyindeng@Meiyins-MacBook-Pro.local>
2020-11-08 11:08:05 -0800

    animation part2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,7 +7,7 @@ import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
 import { trigger, state, style, animate, transition } from '@angular/animations';
-
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 
 
@@ -17,19 +17,16 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.1s ease-in-out'))
-    ])
+    visibility(),
+    flyInOut(),
+    expand()
   ]
+
 })
 export class DishdetailComponent implements OnInit {
     
